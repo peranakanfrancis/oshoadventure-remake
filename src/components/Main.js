51fbc0857c6123c395b2b76d79ccc8b3e125edf7
Peranakan Francis <francis.tch@gmail.com>
2019-07-22 16:40:46 +0545

    push to publish

--- src/components/Main.js
+++ src/components/Main.js
@@ -66,7 +66,10 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="https://formspree.io/xorayqgm">
+          <form method="post" name="contact" netlify-honeypot="bot-field" data-netlify="true" data-netlify-recaptcha="true">
+          <p class="hidden">
+              <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+            </p>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -79,6 +82,7 @@ class Main extends React.Component {
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
+            <div data-netlify-recaptcha="true"></div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
